b37fc81b71fa0af95987d33b1f0aad4675644783
Gargeya Smruti Ranjan Lnu <gargeya_s_r@MacBookAir.home>
2026-02-01 23:03:27 -0800

    demo

--- demo.java
+++ demo.java
@@ -1,10 +1,9 @@
 public class demo {
     public static void main(String[] args) {
-        // print multiplication table 1 through 10
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) {
-                System.out.print(i * j);
-                if (j < 10) System.out.print("\t");
+        for (int n = 1; n <= 12; n++) {
+            System.out.println("Multiplication table for " + n + ":");
+            for (int i = 1; i <= 12; i++) {
+                System.out.printf("%d x %d = %d%n", n, i, n * i);
             }
             System.out.println();
         }
